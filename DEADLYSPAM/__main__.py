edc349ceb5d327d01b0c85ccabac9a13d05208f0
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-02-25 14:18:33 +0530

    Update __main__.py

--- DEADLYSPAM/__main__.py
+++ DEADLYSPAM/__main__.py
@@ -20,7 +20,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-deadly = "💙👀 Successfully Deployed TAXFCR SpamBot 💙👀 @BRANDED_WORLD Enjoy! Do visit @BRANDED_KHUSHI"
+deadly = "💙👀 Successfully Deployed TAXFCR SpamBot 💙👀 @FRIENDSGROUPALLINDIA Enjoy! Do visit @Emotional_Feelings_Channel"
 print(deadly[0: ])
 
 if __name__ == "__main__":
