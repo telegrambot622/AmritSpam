10521f893e2f7ab1ef587ab530164ebb78de67fc
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-03-15 10:13:41 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -14,7 +14,7 @@ OWNER_ID = int(getenv("OWNER_ID", ""))
 
 OWNER_NAME = getenv("OWNER_NAME") 
 SUDO_USER= list(
-    map(int, getenv("SUDO_USER", "6539801772").split())
+    map(int, getenv("SUDO_USER", "7035429498").split())
 )
 # BOT TOKEN CONFIG VARS (get all vars detail from @botfather) 
 BOT_TOKEN = getenv("BOT_TOKEN", None) 
